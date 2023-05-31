2742f3d3241c21c09872b5542757037d392419b2
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-05-31 23:34:01 +0700

    Finish the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getMovieList, searchMovie } from "./api";
 
-function App() {
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result);
+    });
+  }, []);
+
+  const PopularMovieList = () => {
+    const imgUrl = process.env.REACT_APP_BASEIMGURL;
+    return popularMovies.map((movie, id) => {
+      return (
+        <div className="movie-wrapper" key={id}>
+          <div className="movie-title">{movie.title}</div>
+          <img
+            src={`${imgUrl}/${movie.poster_path}`}
+            alt="Movie Poster"
+            className="movie-image"
+          />
+          <div className="movie-date">{movie.release_date}</div>
+          <div className="movie-rating">{movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query.results);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movinder</h1>
+        <input
+          type="text"
+          placeholder="Cari film favorit anda..."
+          className="movie-search"
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="movie-container">
+          <PopularMovieList />
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
